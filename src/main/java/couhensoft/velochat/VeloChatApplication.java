80e28b43597a0ad6c564cd9be1dd23631f55be82
jaehyeok <paosidu@naver.com>
2020-10-29 00:52:24 +0900

    jar 빌드 방법 & 도커 이미지 생성 방법 * dockerfile 추가됨 # 기본 빌드 # ./gradlew build && java -jar build/libs/helloworld-0.1.0.jar # 도커파일 있는 위치에서 실행 # java -Djava.security.edg=file:/dev/./urandom -jar /to-do-springboot.jar

--- src/main/java/couhensoft/velochat/VeloChatApplication.java
+++ src/main/java/couhensoft/velochat/VeloChatApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VeloChatApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("spring.profiles.default", "local");
 		SpringApplication.run(VeloChatApplication.class, args);
 	}
 
